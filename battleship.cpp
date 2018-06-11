5f43959f97667e59a40a6612ae728e0308ec8a81
h <h@hs-MacBook-Pro.attlocal.net>
2018-06-10 20:21:52 -0700

    displayBoards()

--- battleship.cpp
+++ battleship.cpp
@@ -3,8 +3,33 @@
 
 using namespace std;
 
+void displayBoards();//char [][10], char [][10]);
+
+
 int main()
 {
-	cout << "jello\n";
+	displayBoards();
 	return (0);
+}
+void displayBoards()//char a[][10], char b[][10])
+{
+	cout << "                  Your Board          "
+		 << "                                Enemy Board\n"
+		 << "    1   2   3   4   5   6   7   8   9  10"
+		 << "                1   2   3   4   5   6   7   8   9  10\n"
+		 << "  -----------------------------------------"
+		 << "            -----------------------------------------\n";
+
+	// for (size_t i = 0; i < 11; i++)
+	// {
+	// 	cout << " |";
+	// 	for (size_t i = 0; i < 3; i++) {
+	// 		cout << " ";
+	// 	}
+	// 	for (size_t i = 0; i < count; i++) {
+	// 		/* code */
+	// 	}
+	// }
+
+
 }
