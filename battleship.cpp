8d8a73b1b2bd6da788d2eb7c5e02e2796df6b64b
h <h@hs-MacBook-Pro.attlocal.net>
2018-06-10 20:31:19 -0700

    displayBoards()

--- battleship.cpp
+++ battleship.cpp
@@ -13,23 +13,43 @@ int main()
 }
 void displayBoards()//char a[][10], char b[][10])
 {
+	char	A = 'A';
+	string	l1 = "  -----------------------------------------";
+	string	l2 = "            -----------------------------------------\n";
+
 	cout << "                  Your Board          "
 		 << "                                Enemy Board\n"
 		 << "    1   2   3   4   5   6   7   8   9  10"
-		 << "                1   2   3   4   5   6   7   8   9  10\n"
-		 << "  -----------------------------------------"
-		 << "            -----------------------------------------\n";
+		 << "                1   2   3   4   5   6   7   8   9  10\n";
 
-	// for (size_t i = 0; i < 11; i++)
-	// {
-	// 	cout << " |";
-	// 	for (size_t i = 0; i < 3; i++) {
-	// 		cout << " ";
-	// 	}
-	// 	for (size_t i = 0; i < count; i++) {
-	// 		/* code */
-	// 	}
-	// }
+	for (size_t i = 0; i < 11; i++)
+	{
+
+		cout << l1 << l2 << A << ' ';
+		for (size_t i = 0; i < 11; i++)
+		{
+			cout << '|';
+			for (size_t i = 0; i < 3; i++)
+			{
+				cout << ' ';
+			}
+		}
+
+
+		cout << "       " << A++ << " ";
+		for (size_t i = 0; i < 11; i++)
+		{
+			cout << '|';
+			for (size_t i = 0; i < 3; i++)
+			{
+				cout << ' ';
+			}
+		}
+		cout << endl;
+
+	}
+
+	cout << l1 << l2;
 
 
 }
